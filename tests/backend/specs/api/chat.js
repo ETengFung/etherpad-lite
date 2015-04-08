f2f73678ad65f5281280c779f3faace6ec6be5d4
webzwo0i <webzwo0i@c3d2.de>
2015-04-08 04:38:50 +0200

    pad names in api-tests contain surrogates

--- tests/backend/specs/api/chat.js
+++ tests/backend/specs/api/chat.js
@@ -123,7 +123,7 @@ function makeid()
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for( var i=0; i < 5; i++ ){
+  for( var i=0; i < 3; i++ ){
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text+"ｙ";
