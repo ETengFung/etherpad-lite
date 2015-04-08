daf218a9e8aecdb4e45049172c07fda2a376fddd
webzwo0i <webzwo0i@c3d2.de>
2015-04-08 04:38:50 +0200

    pad names in api-tests contain surrogates

--- tests/backend/specs/api/pad.js
+++ tests/backend/specs/api/pad.js
@@ -509,10 +509,10 @@ function makeid()
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for( var i=0; i < 5; i++ ){
+  for( var i=0; i < 3; i++ ){
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return text;
+  return text+"\uD83C\uDF15";
 }
 
 function generateLongText(){
