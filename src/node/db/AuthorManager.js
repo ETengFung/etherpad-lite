09c29c968988b65b6a0fd2ff531e4b644565b756
webzwo0i <webzwo0i@c3d2.de>
2015-04-14 14:28:21 +0200

    style

--- src/node/db/AuthorManager.js
+++ src/node/db/AuthorManager.js
@@ -126,7 +126,7 @@ exports.createAuthor = function(name, callback)
   // code points above U+FFFF
   if(name && name.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/))
   {
-    callback(new customError("author with unrepresentable character"));
+    callback("author with unrepresentable character");
   }
   //create the new author name
   var author = "a." + randomString(16);
