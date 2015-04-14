972633940723450e0291a5a80e8e5870efed650a
webzwo0i <webzwo0i@c3d2.de>
2015-04-14 14:27:56 +0200

    need to replace chars in appendChatMessage, not handleChatMessage

--- tests/backend/specs/api/chat.js
+++ tests/backend/specs/api/chat.js
@@ -98,7 +98,16 @@ describe('getChatHistory', function(){
     .expect(function(res){
       if(res.body.data.messages.length !== 3) throw new Error("Chat History Length is wrong");
       if(res.body.code !== 0) throw new Error("Unable to get chat history");
-      if(res.body.data.messages.first.last.text !== "\uFFFD\uFFFD") throw new Error("char in chat message with unicode above U+FFFF was not replaced");
+    })
+    .expect('Content-Type', /json/)
+    .expect(200, done)
+  });
+  it('Gets the last chat message (with replaced character)', function(done) {
+    api.get(endPoint('getChatHistory')+"&padID="+padID+"&start=1&end=2")
+    .expect(function(res){
+      if(res.body.data.messages.length !== 2) throw new Error("Chat History Length is wrong");
+      if(res.body.code !== 0) throw new Error("Unable to get chat history");
+      if(res.body.data.messages[1].text !== "\uFFFD\uFFFD") throw new Error("char in chat message with unicode above U+FFFF was not replaced");
     })
     .expect('Content-Type', /json/)
     .expect(200, done)
