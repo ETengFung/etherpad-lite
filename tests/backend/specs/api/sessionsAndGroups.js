49d3d4e977a45a6531f0aa1344f0431c12e9702c
webzwo0i <webzwo0i@c3d2.de>
2015-04-14 18:29:20 +0200

    increase generated pad length in api-tests to the old value+one special char

--- tests/backend/specs/api/sessionsAndGroups.js
+++ tests/backend/specs/api/sessionsAndGroups.js
@@ -421,7 +421,7 @@ function makeid()
   var text = "";
   var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for( var i=0; i < 4; i++ ){
+  for( var i=0; i < 5; i++ ){
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text+"\uD83C\uDCDFｙ\uD83C\uDCDF";
