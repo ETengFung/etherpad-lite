45fd454cf9871b1bfa057957651213e4a63f2b13
webzwo0i <webzwo0i@c3d2.de>
2015-04-14 13:31:14 +0200

    fix

--- src/node/db/AuthorManager.js
+++ src/node/db/AuthorManager.js
@@ -124,7 +124,7 @@ function mapAuthorWithDBKey (mapperkey, mapper, callback)
 exports.createAuthor = function(name, callback)
 {
   // code points above U+FFFF
-  if(name.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/))
+  if(name && name.match(/[\uD800-\uDBFF][\uDC00-\uDFFF]/))
   {
     callback(new customError("author with unrepresentable character"));
   }
