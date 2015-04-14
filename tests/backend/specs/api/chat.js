3354bc19ae8e1aa59945527aded270d7013fb67e
webzwo0i <webzwo0i@c3d2.de>
2015-04-14 13:54:23 +0200

    debug

--- tests/backend/specs/api/chat.js
+++ tests/backend/specs/api/chat.js
@@ -98,7 +98,7 @@ describe('getChatHistory', function(){
     .expect(function(res){
       if(res.body.data.messages.length !== 3) throw new Error("Chat History Length is wrong");
       if(res.body.code !== 0) throw new Error("Unable to get chat history");
-      console.error("last:",res.body.data.messages.last.text);
+      console.error("chat msgs:",res.body.data.messages);
       if(res.body.data.messages.last.text !== "\uFFFD\uFFFD") throw new Error("char in chat message with unicode above U+FFFF was not replaced");
     })
     .expect('Content-Type', /json/)
