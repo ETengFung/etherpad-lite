54cf66a6d3ba291d6847e71cec2fad0e5815eb30
webzwo0i <webzwo0i@c3d2.de>
2015-04-19 01:25:07 +0200

    backend tests UTF8 (chat messages)

--- tests/backend/specs/api/chat.js
+++ tests/backend/specs/api/chat.js
@@ -37,6 +37,8 @@ describe('API Versioning', function(){
     -> getChatHistory(padID)
 */
 
+//TODO test against single surrogates
+//TODO test against chars outside BMP
 describe('createPad', function(){
   it('creates a new Pad', function(done) {
     api.get(endPoint('createPad')+"&padID="+padID)
@@ -48,6 +50,8 @@ describe('createPad', function(){
   });
 })
 
+//TODO test against single surrogates
+//TODO test against chars outside BMP
 describe('createAuthor', function(){
   it('Creates an author with a name set', function(done) {
     api.get(endPoint('createAuthor'))
@@ -61,10 +65,34 @@ describe('createAuthor', function(){
 })
 
 describe('appendChatMessage', function(){
-  it('Adds a chat message to the pad', function(done) {
-    api.get(endPoint('appendChatMessage')+"&padID="+padID+"&text=blalblalbha&authorID="+authorID+"&time="+timestamp)
+  it('Adds a chat message to the pad (BMP)', function(done) {
+    api.get(endPoint('appendChatMessage')+"&padID="+padID+"&text=ｙ&authorID="+authorID+"&time="+timestamp)
     .expect(function(res){
       if(res.body.code !== 0) throw new Error("Unable to create chat message");
+    })
+    .expect('Content-Type', /json/)
+    .expect(200, done)
+  });
+  it('Adds a chat message outside BMP and replaces surrogates with U+FFFF', function(done) {
+    api.get(endPoint('appendChatMessage')+"&padID="+padID+"&text=\uD835\uDC00&authorID="+authorID+"&time="+timestamp)
+    .expect(function(res){
+      if(res.body.code !== 0) throw new Error("Cannot send chat message with unicode above U+FFFF");
+    })
+    .expect('Content-Type', /json/)
+    .expect(200, done)
+  });
+  it('Cannot send a chat message with single high surrogate', function(done) {
+    api.get(endPoint('appendChatMessage')+"&padID="+padID+"&text=\uD835&authorID="+authorID+"&time="+timestamp)
+    .expect(function(res){
+      if(res.body.code !== 1) throw new Error("Can send chat message with high surrogate");
+    })
+    .expect('Content-Type', /json/)
+    .expect(200, done)
+  });
+  it('Cannot send a chat message with single low surrogate', function(done) {
+    api.get(endPoint('appendChatMessage')+"&padID="+padID+"&text=\uDC00&authorID="+authorID+"&time="+timestamp)
+    .expect(function(res){
+      if(res.body.code !== 1) throw new Error("Can send chat message with low surrogate");
     })
     .expect('Content-Type', /json/)
     .expect(200, done)
@@ -76,8 +104,7 @@ describe('getChatHead', function(){
   it('Gets the head of chat', function(done) {
     api.get(endPoint('getChatHead')+"&padID="+padID)
     .expect(function(res){
-      if(res.body.data.chatHead !== 0) throw new Error("Chat Head Length is wrong");
-
+      if(res.body.data.chatHead !== 1) throw new Error("Chat Head Length is wrong");
       if(res.body.code !== 0) throw new Error("Unable to get chat head");
     })
     .expect('Content-Type', /json/)
@@ -86,11 +113,14 @@ describe('getChatHead', function(){
 })
 
 describe('getChatHistory', function(){
+  var chat_history;
   it('Gets Chat History of a Pad', function(done) {
     api.get(endPoint('getChatHistory')+"&padID="+padID)
     .expect(function(res){
-      if(res.body.data.messages.length !== 1) throw new Error("Chat History Length is wrong");
+      chat_history = res.body.data.messages;
       if(res.body.code !== 0) throw new Error("Unable to get chat history");
+      if(chat_history.length !== 2) throw new Error("Chat History Length is wrong");
+      if(chat_history[1].text !== "\uFFFD\uFFFD") throw new Error("Unicode above U+FFFF was not replaced");
     })
     .expect('Content-Type', /json/)
     .expect(200, done)
@@ -109,5 +139,5 @@ function makeid()
   for( var i=0; i < 5; i++ ){
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
-  return text;
+  return text+"ｙ";
 }
