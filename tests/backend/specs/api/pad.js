54840baf75c2279317b288f64cacc857fdd85c7f
webzwo0i <webzwo0i@c3d2.de>
2015-04-19 04:14:01 +0200

    add partial test for case-sensitivity

--- tests/backend/specs/api/pad.js
+++ tests/backend/specs/api/pad.js
@@ -102,6 +102,39 @@ describe('createPad', function(){
     .expect('Content-Type', /json/)
     .expect(200, done)
   });
+
+  //TODO
+  //test should reflect what happens with databases that have case-insensitive
+  //collations
+  it('creates a new Pad with different case', function(done) {
+    var index;
+    if(index = testPadId[1].match(/[a-zA-Z]/)){
+      if(testPadId[1].match(/[a-z]/)){
+        testPadId[1] = testPadId[1].toUpperCase();
+      } else {
+        testPadId[1] = testPadId[1].toLowerCase();
+      }
+    } else {
+      console.error('could not test createPad with different case because only numbers where used')
+    }
+    api.get(endPoint('createPad')+"&padID="+testPadId)
+    .expect(function(res){
+      if(res.body.message === 'padID does already exist') throw new Error("unable to create pad with different name");
+      if(res.body.code !== 0) throw new Error("Unable to create new Pad");
+    })
+    .expect('Content-Type', /json/)
+    .expect(200, done)
+  });
+  // raises URIError
+  //it('cannot create a new Pad with broken astral', function(done) {
+  //  api.get(endPoint('createPad')+"&padID="+padID+"\uD83C")
+  //  .expect(function(res){
+  //    console.error(res.body)
+  //    if(res.body.code !== 0) throw new Error("Pad created with broken astral");
+  //  })
+  //  .expect('Content-Type', /json/)
+  //  .expect(200, done)
+  //});
 })
 
 describe('getRevisionsCount', function(){
