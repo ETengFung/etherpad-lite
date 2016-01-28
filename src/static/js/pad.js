9feb5eb7d8817a41b1dbbdfd0888e1b61a0254cd
webzwo0i <webzwo0i@c3d2.de>
2016-01-28 10:57:52 +0100

    Revert "seems to work"
    This reverts commit f16616d23bbbf813232bfa80f2796dca1eaf072c.
    
    Conflicts:
    	src/node/handler/ImportHandler.js
    	src/static/js/pad.js
    	src/templates/pad.html

--- src/static/js/pad.js
+++ src/static/js/pad.js
@@ -521,6 +521,11 @@ var pad = {
     pad.initTime = +(new Date());
     pad.padOptions = clientVars.initialOptions;
 
+    if ((!browser.msie) && (!(browser.firefox && browser.version.indexOf("1.8.") == 0)))
+    {
+      document.domain = document.domain; // for comet
+    }
+
     // for IE
     if (browser.msie)
     {
