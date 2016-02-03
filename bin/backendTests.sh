2007a2c95cf0f1b98ab20cb57131b5a4f3f3adfb
webzwo0i <webzwo0i@c3d2.de>
2016-02-03 18:34:00 +0100

    always run new tests via backendTests.sh

--- bin/backendTests.sh
+++ bin/backendTests.sh
@@ -1,1 +1,1 @@
-src/node_modules/mocha/bin/mocha --timeout 5000 --reporter nyan tests/backend/specs/api
+src/node_modules/mocha/bin/mocha --timeout 5000 --reporter nyan tests/backend/specs/api tests/backend/specs/messages
