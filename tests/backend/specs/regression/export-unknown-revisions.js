9dc08060f3d0285a8376c20db83aeaf7fa3136db
webzwo0i <webzwo0i@c3d2.de>
2016-02-15 00:23:53 +0100

    make padid for export regressions random expect 400 status code on failure

--- tests/backend/specs/regression/export-unknown-revisions.js
+++ tests/backend/specs/regression/export-unknown-revisions.js
@@ -5,101 +5,49 @@ var assert = require('assert')
        eplserver = supertest('http://'+settings.ip+":"+settings.port),
        eplws = supertest('ws://'+settings.ip+":"+settings.port),
       path = require('path'),
-     async = require(__dirname+'/../../../../src/node_modules/async'),
-    // helper = require(__dirname+'/helper.js')
+     async = require(__dirname+'/../../../../src/node_modules/async');
+
+var padid = makeid();
 
 describe('Export txt',function(){
-  xit('should not crash on unknown revision',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/2/export/txt')
+  it('should not crash on unknown revision',function(done){
+    eplserver.get('/p/'+padid+'/2/export/txt')
     .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
     })
-    .expect(200, done)
+    .expect(400, done)
   });
-  xit('should not crash on undefined revision',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf//export/txt')
+  it('should not crash on null-string revision',function(done){
+    eplserver.get('/p/'+padid+'/null/export/txt')
     .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
     })
-    .expect(404,done);
+    .expect(400,done);
   });
-  xit('should not crash when revision is a string',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/undefined/export/txt')
+  it('should not crash on undefined-string revision',function(done){
+    eplserver.get('/p/'+padid+'/undefined/export/txt')
     .expect(function(res){
-      if(typeof res.text !== 'string') throw new Error("res is not string")
+      if(res.text !== '') throw new Error("res is not empty")
     })
-    .expect(200,done)
+    .expect(400,done)
   });
-  xit('should not crash when revision is a string',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/fooo/export/txt')
+  it('should not crash if revision is a normal string',function(done){
+    eplserver.get('/p/'+padid+'/fooo/export/txt')
     .expect(function(res){
-      if(typeof res.text !== 'string') throw new Error("res is not string")
+      if(res.text !== '') throw new Error("res is not empty")
     })
-    .expect(200,done)
-  });
-
-  xit('should not crash when revision is empty string',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/""/export/txt')
-    .expect(function(res){
-      if(typeof res.text !== 'string') throw new Error("res is not string")
-    })
-    .expect(200,done)
-  });
-  xit('should always create a new pad',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/0/export/txt')
-    .expect(function(res){
-      if(!res.text || res.text.length === 0){
-        throw new Error("empty default text or undefined text?")
-      }
-    })
-    .expect(200, done)
-  });
-  xit('should fail when headrev + 1 is requested',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/10/export/txt')
-    .expect(function(res){
-      if(!res.text || res.text.length === 0){
-        throw new Error("empty default text or undefined text?")
-      }
-    })
-    .expect(200, done)
-  });
-});
-//some as above :-)
-describe('Export html',function(){
-  xit('should not crash on unknown revision',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/2/export/html')
-    .expect(function(res){
-    })
-    .expect(200, done)
-  });
-  it('should not crash on undefined revision',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf//export/html')
-    .expect(function(res){
-    })
-    .expect(404,done);
-  });
-  it('should not crash when revision is a string',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/undefined/export/html')
-    .expect(function(res){
-      if(typeof res.text !== 'string') throw new Error("res is not string")
-    })
-    .expect(200,done)
-  });
-  it('should not crash when revision is a string',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/fooo/export/html')
-    .expect(function(res){
-      if(typeof res.text !== 'string') throw new Error("res is not string")
-    })
-    .expect(200,done)
+    .expect(400,done)
   });
 
   it('should not crash when revision is empty string',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/""/export/html')
+    eplserver.get('/p/'+padid+'/""/export/txt')
     .expect(function(res){
-      if(typeof res.text !== 'string') throw new Error("res is not string")
+      if(res.text !== '') throw new Error("res is not empty")
     })
-    .expect(200,done)
+    .expect(400,done)
   });
   it('should always create a new pad',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/0/export/html')
+    eplserver.get('/p/'+padid+'/0/export/txt')
     .expect(function(res){
       if(!res.text || res.text.length === 0){
         throw new Error("empty default text or undefined text?")
@@ -108,13 +56,90 @@ describe('Export html',function(){
     .expect(200, done)
   });
   it('should fail when headrev + 1 is requested',function(done){
-    eplserver.get('/p/dsfjsdknfsdjkfnsdjfnsdf/10/export/html')
+    eplserver.get('/p/'+padid+'/1/export/txt')
     .expect(function(res){
-//      console.log(res)
+      if(res.text !== '') throw new Error("res is not empty")
+    })
+    .expect(400, done)
+  });
+  it('should fail when headrev + 2 is requested',function(done){
+    eplserver.get('/p/'+padid+'/2/export/txt')
+    .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
+    })
+    .expect(400, done)
+  });
+});
+//some as above :-)
+describe('Export html',function(){
+  it('should not crash on unknown revision',function(done){
+    eplserver.get('/p/'+padid+'/2/export/html')
+    .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
+    })
+    .expect(400, done)
+  });
+  it('should not crash on null-string revision',function(done){
+    eplserver.get('/p/'+padid+'/null/export/html')
+    .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
+    })
+    .expect(400,done);
+  });
+  it('should not crash on undefined-string revision',function(done){
+    eplserver.get('/p/'+padid+'/undefined/export/html')
+    .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
+    })
+    .expect(400,done)
+  });
+  it('should not crash if revision is a normal string',function(done){
+    eplserver.get('/p/'+padid+'/fooo/export/html')
+    .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
+    })
+    .expect(400,done)
+  });
+
+  it('should not crash when revision is empty string',function(done){
+    eplserver.get('/p/'+padid+'/""/export/html')
+    .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
+    })
+    .expect(400,done)
+  });
+  it('should always create a new pad',function(done){
+    eplserver.get('/p/'+padid+'/0/export/html')
+    .expect(function(res){
       if(!res.text || res.text.length === 0){
         throw new Error("empty default text or undefined text?")
       }
     })
     .expect(200, done)
   });
+  it('should fail when headrev + 1 is requested',function(done){
+    eplserver.get('/p/'+padid+'/1/export/html')
+    .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
+    })
+    .expect(400, done)
+  });
+  it('should fail when headrev + 2 is requested',function(done){
+    eplserver.get('/p/'+padid+'/2/export/html')
+    .expect(function(res){
+      if(res.text !== '') throw new Error("res is not empty")
+    })
+    .expect(400, done)
+  });
 });
+
+function makeid()
+{
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for( var i=0; i < 40; i++ ){
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
