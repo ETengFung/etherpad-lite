118a360253a8c02c7efc54151800248b8e5703fc
webzwo0i <webzwo0i@c3d2.de>
2017-10-07 14:11:59 +0200

    simplify runner.sh

--- tests/frontend/travis/runner.sh
+++ tests/frontend/travis/runner.sh
@@ -4,14 +4,9 @@
 cd `dirname $0`
 
 #start Etherpad
-../../../bin/run.sh > /tmp/epl-run.log &
+../../../bin/run.sh > /tmp/epl-run.log
 
 #start remote runner
 node remote_runner.js
-exit_code=$?
 
-kill $!
-kill $(cat /tmp/sauce.pid)
-sleep 30
-
-exit $exit_code
+exit $?
